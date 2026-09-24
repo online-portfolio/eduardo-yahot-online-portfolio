f9e26e7b895fcca0d79a16a0b87a32c5fa8c3431
online-portfolio <melfie.va.practice@gmail.com>
2026-09-24 21:39:31 +0800

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -8,6 +8,7 @@ export const getRouter = () => {
   const router = createRouter({
     routeTree,
     context: { queryClient },
+    basepath: "/eduardo-yahot-online-portfolio",
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   });
